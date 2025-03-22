6e7f476c3a382ce7f77d23692a46d3e96d15f363
Rajkumar Ravadi <91647639+RajkumarRavadi@users.noreply.github.com>
2025-03-22 08:27:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
 const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 
 const divide = (arr, n) => {
-  // Write your code here
+  //create a result array
+	let result = [];
+	let chunkArray = [];
+	let chunkSum = 0;
+
+	//start travering the array
+	for(int i=0; i<n; i++){
+		if(chunkSum + arr[i] <= n){
+			chunkArray.push(arr[i]);
+			chunkSum = chunkSum + arr[i];
+		}else{
+			result.push(chunkArray);
+			chunkArray = arr[i];
+			chunkSum = arr[i];
+		}
+	}
+
+	result.push(chunkArray);
+	return result;
 };
 
-const n = prompt("Enter n: ");
-alert(JSON.stringify(divide(arr, n)));
+// const n = prompt("Enter n: ");
+// alert(JSON.stringify(divide(arr, n)));
